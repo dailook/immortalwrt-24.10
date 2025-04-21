97701c2e29b9d134e3bbe59ed992701bb7e79861
dailook <1296762908@qq.com>
2025-04-21 18:45:56 +0800

    Update platform.sh

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -80,8 +80,7 @@ platform_do_upgrade() {
 	h3c,magic-nx30-pro|\
 	imou,lc-hx3001|\
         ikuai,q6000|\
-	jcg,q30-pro|\
-	jdcloud,re-cp-03|\
+	jcg,q30-pro|\	
 	konka,komi-a31|\
 	livinet,zr-3020-ubootmod|\
 	mediatek,mt7981-rfb|\
@@ -115,6 +114,7 @@ platform_do_upgrade() {
 	glinet,gl-xe3000|\
 	huasifei,wh3000-emmc|\
         ikuai,q6000-emmc|\
+	jdcloud,re-cp-03|\
 	smartrg,sdg-8612|\
 	smartrg,sdg-8614|\
 	smartrg,sdg-8622|\
