6bc0a99e4cfbfa8d663fbbb393b531994ee3ef34
dailook <1296762908@qq.com>
2025-04-26 14:42:51 +0800

    Update platform.sh

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -115,6 +115,7 @@ platform_do_upgrade() {
 	huasifei,wh3000-emmc|\
         ikuai,q6000-emmc|\
 	jdcloud,re-cp-03|\
+        sl,3000-emmc|\
 	smartrg,sdg-8612|\
 	smartrg,sdg-8614|\
 	smartrg,sdg-8622|\
@@ -225,6 +226,7 @@ platform_copy_config() {
 	huasifei,wh3000-emmc|\
         ikuai,q6000-emmc|\
 	jdcloud,re-cp-03|\
+        sl,3000-emmc|\
 	smartrg,sdg-8612|\
 	smartrg,sdg-8614|\
 	smartrg,sdg-8622|\
