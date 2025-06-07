ecc8aa8af4856bc4efd4a2407d37e0c758644331
dailook <1296762908@qq.com>
2025-06-07 20:42:48 +0800

    同步

--- target/linux/realtek/files-6.6/drivers/net/dsa/rtl83xx/dsa.c
+++ target/linux/realtek/files-6.6/drivers/net/dsa/rtl83xx/dsa.c
@@ -919,43 +919,43 @@ static void rtl93xx_phylink_mac_link_up(struct dsa_switch *ds, int port,
 	struct dsa_port *dp = dsa_to_port(ds, port);
 	struct rtl838x_switch_priv *priv = ds->priv;
 	u32 mcr, spdsel;
- 
- 	if (speed == SPEED_10000)
- 		spdsel = RTL_SPEED_10000;
- 	else if (speed == SPEED_5000)
- 		spdsel = RTL_SPEED_5000;
- 	else if (speed == SPEED_2500)
- 		spdsel = RTL_SPEED_2500;
- 	else if (speed == SPEED_1000)
- 		spdsel = RTL_SPEED_1000;
- 	else if (speed == SPEED_100)
- 		spdsel = RTL_SPEED_100;
- 	else
- 		spdsel = RTL_SPEED_10;
- 
- 	mcr = sw_r32(priv->r->mac_force_mode_ctrl(port));
- 
- 	if (priv->family_id == RTL9300_FAMILY_ID) {
- 		mcr &= ~RTL930X_RX_PAUSE_EN;
- 		mcr &= ~RTL930X_TX_PAUSE_EN;
- 		mcr &= ~RTL930X_DUPLEX_MODE;
- 		mcr &= ~RTL930X_SPEED_MASK;
- 		mcr |= RTL930X_FORCE_LINK_EN;
- 		mcr |= spdsel << RTL930X_SPEED_SHIFT;
- 
- 		if (tx_pause)
- 			mcr |= RTL930X_TX_PAUSE_EN;
- 		if (rx_pause)
- 			mcr |= RTL930X_RX_PAUSE_EN;
- 		if (duplex == DUPLEX_FULL || priv->lagmembers & BIT_ULL(port))
- 			mcr |= RTL930X_DUPLEX_MODE;
- 		if (dsa_port_is_cpu(dp) || !priv->ports[port].phy_is_integrated)
- 			mcr |= RTL930X_FORCE_EN;
- 	}
- 
- 	pr_debug("%s port %d, mode %x, speed %d, duplex %d, txpause %d, rxpause %d: set mcr=%08x\n",
- 		__func__, port, mode, speed, duplex, tx_pause, rx_pause, mcr);
- 	sw_w32(mcr, priv->r->mac_force_mode_ctrl(port));
+
+	if (speed == SPEED_10000)
+		spdsel = RTL_SPEED_10000;
+	else if (speed == SPEED_5000)
+		spdsel = RTL_SPEED_5000;
+	else if (speed == SPEED_2500)
+		spdsel = RTL_SPEED_2500;
+	else if (speed == SPEED_1000)
+		spdsel = RTL_SPEED_1000;
+	else if (speed == SPEED_100)
+		spdsel = RTL_SPEED_100;
+	else
+		spdsel = RTL_SPEED_10;
+
+	mcr = sw_r32(priv->r->mac_force_mode_ctrl(port));
+
+	if (priv->family_id == RTL9300_FAMILY_ID) {
+		mcr &= ~RTL930X_RX_PAUSE_EN;
+		mcr &= ~RTL930X_TX_PAUSE_EN;
+		mcr &= ~RTL930X_DUPLEX_MODE;
+		mcr &= ~RTL930X_SPEED_MASK;
+		mcr |= RTL930X_FORCE_LINK_EN;
+		mcr |= spdsel << RTL930X_SPEED_SHIFT;
+
+		if (tx_pause)
+			mcr |= RTL930X_TX_PAUSE_EN;
+		if (rx_pause)
+			mcr |= RTL930X_RX_PAUSE_EN;
+		if (duplex == DUPLEX_FULL || priv->lagmembers & BIT_ULL(port))
+			mcr |= RTL930X_DUPLEX_MODE;
+		if (dsa_port_is_cpu(dp) || !priv->ports[port].phy_is_integrated)
+			mcr |= RTL930X_FORCE_EN;
+	}
+
+	pr_debug("%s port %d, mode %x, speed %d, duplex %d, txpause %d, rxpause %d: set mcr=%08x\n",
+		__func__, port, mode, speed, duplex, tx_pause, rx_pause, mcr);
+	sw_w32(mcr, priv->r->mac_force_mode_ctrl(port));
 
 	/* Restart TX/RX to port */
 	sw_w32_mask(0, 0x3, priv->r->mac_port_ctrl(port));
